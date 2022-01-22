e1900d5461229c6f7f061e4b916ac45fe4e33843
daniele.bonfanti <bonfantidaniele94@gmail.com>
2022-01-22 12:24:00 +0100

    Add new post component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewPostComponent } from './new-post/new-post.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
 
-const routes: Routes = [{ path: '', component: PostsListComponent }];
+const routes: Routes = [
+  { path: '', component: PostsListComponent },
+  { path: 'new-post', component: NewPostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
