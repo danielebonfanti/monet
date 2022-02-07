078429f8e1850d1c86edda6e5d6b67c9cc253b27
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2022-02-07 17:35:11 +0100

    Add login interface

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LoginRoutingModule } from './login-routing.module';
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, MatInputModule, MatIconModule, MatButtonModule],
+  exports: [LoginComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
