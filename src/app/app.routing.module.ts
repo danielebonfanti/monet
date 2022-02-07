7c5f17e89e3bc4dc6962b31dfb8c5cb71cefa570
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2022-02-07 18:24:56 +0100

    Add login page and authentication

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsListComponent } from './posts-list/posts-list.component';
 
-const routes: Routes = [{ path: '', component: PostsListComponent }];
+const routes: Routes = [
+  { path: '', component: PostsListComponent },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
