3021ab8b53e63f0ccbe4431040f5acf9eda39441
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2022-03-16 22:24:52 +0100

    Change color theme to white by default (#7)
    * Change to white navbar e theme
    
    * Formatting

--- src/app/header/monet-header.component.ts
+++ src/app/header/monet-header.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'monet-header',
   templateUrl: './monet-header.component.html',
   styleUrls: ['./monet-header.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
-export class MonetHeaderComponent implements OnInit {
+export class MonetHeaderComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
