92c0fa5cb2c4cb9d13b7fdbe673ff708531251ad
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2022-09-17 21:50:33 +0200

    Clean home page

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'The darkside of the code';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
